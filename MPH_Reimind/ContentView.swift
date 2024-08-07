a6d475bdefd2903b784f7337293e59869714fc6c
OtoneChan <konpota1175@gmail.com>
2024-08-07 19:26:23 +0900

    Initial Commit

---
+++ MPH_Reimind/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MPH_Reimind
+//
+//  Created by 高橋涼太 on 2024/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MPH_Reimind/MPH_ReimindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MPH_ReimindApp.swift
+//  MPH_Reimind
+//
+//  Created by 高橋涼太 on 2024/08/07.
+//
+
+import SwiftUI
+
+@main
+struct MPH_ReimindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
